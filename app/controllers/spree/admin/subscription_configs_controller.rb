2978bd5aa53fa04180ba7611ced03248849767db
Daniyal Basit <dbk590@gmail.com>
2021-06-16 15:44:37 +0500

    added config form

--- app/controllers/spree/admin/subscription_configs_controller.rb
+++ app/controllers/spree/admin/subscription_configs_controller.rb
@@ -1,7 +1,13 @@
 module Spree
   module Admin
     class SubscriptionConfigsController < Spree::Admin::BaseController
-      def index; end
+      def index
+        @config = Spree::SubscriptionConfig.last || Spree::SubscriptionConfig.create(max_subscribers: 100)
+      end
+
+      def update
+        puts params
+      end
     end
   end
 end
