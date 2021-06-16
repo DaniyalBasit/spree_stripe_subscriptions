bc1cbc5529ac7d9cb26f5ec5c7f235ceb13fcc30
Daniyal Basit <dbk590@gmail.com>
2021-06-16 15:15:14 +0500

    fixed query selector

--- app/overrides/add_subscription_configs_in_side_menu.rb
+++ app/overrides/add_subscription_configs_in_side_menu.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_main_menu',
   name: 'add_stripe_info_to_user_edit',
-  insert_after: "div#sidebarProduct",
+  insert_after: "ul#sidebarProduct",
   text: "
     <ul class='nav nav-sidebar border-bottom' id='sidebarSubscriptionConfig'>
       <%= main_menu_tree(
