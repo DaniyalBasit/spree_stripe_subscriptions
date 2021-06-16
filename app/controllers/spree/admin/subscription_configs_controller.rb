af792bf940283b344b510ccc7ab11227a0ea0805
Daniyal Basit <dbk590@gmail.com>
2021-06-16 14:54:42 +0500

    added configs controller

---
+++ app/controllers/spree/admin/subscription_configs_controller.rb
@@ -0,0 +1,7 @@
+module Spree
+  module Admin
+    class SubscriptionConfigsController < Spree::Admin::BaseController
+      def index; end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
-Spree::Core::Engine.add_routes do
-  # Add your extension routes here
+Spree::Core::Engine.routes.draw do
+  namespace :admin do
+    resources :subscription_configs, only: [:index]
+  end
 end
