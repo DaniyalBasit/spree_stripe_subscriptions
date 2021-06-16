8d3fd48928b271e239972d24c733834c139baa85
Daniyal Basit <dbk590@gmail.com>
2021-06-16 15:11:05 +0500

    added subscription configs in the main menu of the admin panel

---
+++ app/overrides/add_subscription_configs_in_side_menu.rb
@@ -0,0 +1,15 @@
+Deface::Override.new(
+  virtual_path: 'spree/admin/shared/_main_menu',
+  name: 'add_stripe_info_to_user_edit',
+  insert_after: "div#sidebarProduct",
+  text: "
+    <ul class='nav nav-sidebar border-bottom' id='sidebarSubscriptionConfig'>
+      <%= main_menu_tree(
+        'Subscription Configs',
+        icon: 'products.svg',
+        sub_menu: 'subscription_config',
+        url: admin_subscription_configs_path)
+      %>
+    </ul>
+  "
+)
