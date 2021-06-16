dd1edd2eb17f7edba03cb076e7aed894563ad664
Daniyal Basit <dbk590@gmail.com>
2021-06-16 15:47:19 +0500

    updated route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
-    resources :subscription_configs, only: [:index]
+    resources :subscription_configs, only: [:index, :update]
   end
 end
